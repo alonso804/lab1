a41ef055332f8ac672ce5f711de61e3ad567f162
alonso804 <alonsobs804@gmail.com>
2021-09-25 14:17:57 -0500

    add some changes

--- spatial/BasicSpatial.hpp
+++ spatial/BasicSpatial.hpp
@@ -4,21 +4,20 @@
 
 namespace utec {
 namespace spatial {
-
 /**
  * BasicSpatial implementation
  */
 template <typename Point>
 class BasicSpatial : public SpatialBase<Point> {
  private:
-
  public:
-  BasicSpatial() {};
+  BasicSpatial(){};
   void insert(const Point& new_point) override {}
 
   // El punto de referencia no necesariamente es parte del dataset
-  Point nearest_neighbor(const Point& reference) override { return Point({0,0}); }
+  Point nearest_neighbor(const Point& reference) override {
+    return Point({0, 0});
+  }
 };
-
 }  // namespace spatial
 }  // namespace utec
